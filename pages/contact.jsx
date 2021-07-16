9a8db41261a10bd0238bd5fc5993ec7c08c25b0a
bluuweb <contacto@bluuweb.cl>
2021-07-16 11:31:32 -0400

    primerazo commit

---
+++ pages/contact.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function contact() {
+  return (
+    <Layout title="Contact | next.js" description="agregue una descripcion">
+      <h1>Contact</h1>
+    </Layout>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout
+      title="Home | Next.js"
+      description="esta es la descripcion del home"
+      home
+    >
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
